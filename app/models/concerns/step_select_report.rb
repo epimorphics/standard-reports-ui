4177cf8e3ff5a67422797606582014bcb8bfa5d6
Ian Dickinson <ian@epimorphics.com>
2015-11-24 14:23:01 +0000

    Starting to build out the Step API with support for the values to be chosen at a step

--- app/models/concerns/step_select_report.rb
+++ app/models/concerns/step_select_report.rb
@@ -2,6 +2,13 @@
 
 class StepSelectReport < Step
   def initialize
-    super( :select_report )
+    super( :select_report, :rt )
+  end
+
+  def values
+    [
+      Struct::StepValue.new( "Average prices and volumes", :avg_price ),
+      Struct::StepValue.new( "Banded prices", :banded_price )
+    ]
   end
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,9 +1,16 @@
 # Base class for a workflow step
 
-class Step
-  attr_reader :name
+Struct.new( "StepValue", :label, :value )
 
-  def initialize( name )
+class Step
+  attr_reader :name, :param_name
+
+  def initialize( name, param_name )
     @name = name
+    @param_name = param_name
+  end
+
+  def completed?( params )
+    params.has_key?( param_name )
   end
 end
