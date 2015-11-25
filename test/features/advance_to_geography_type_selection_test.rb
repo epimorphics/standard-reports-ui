d36a50a9b4ef4248e6767c6ce3e255e06731efff
Ian Dickinson <i.j.dickinson@gmail.com>
2015-11-25 15:45:34 +0000

    Add test for feature of moving to geography type selection

---
+++ test/features/advance_to_geography_type_selection_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+feature "can advance to geography type select" do
+  scenario "select report type and submit" do
+    visit report_design_path
+    choose "Average prices and volumes"
+    click_button "Next"
+    page.must_have_css( ".select_geography_type" )
+  end
+end
