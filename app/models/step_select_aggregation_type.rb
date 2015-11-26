910676d8680c6d500ba194e0723e0491875fdc70
Ian Dickinson <i.j.dickinson@gmail.com>
2015-11-26 12:23:51 +0000

    Add step to let user choose aggregation level. So far only works for countries and avg price reports

---
+++ app/models/step_select_aggregation_type.rb
@@ -0,0 +1,40 @@
+# Workflow step of selecting an aggregation type
+
+class StepSelectAggregationType < Step
+  AGGREGATE_BY_REGION = Struct::StepValue.new( "Region", :region )
+  AGGREGATE_BY_COUNTY = Struct::StepValue.new( "County, Unitary Authority", :county )
+  AGGREGATE_BY_DISTRICT = Struct::StepValue.new( "District or London Borough", :district )
+  AGGREGATE_BY_PC_AREA = Struct::StepValue.new( "Postcode area", :pcArea )
+  AGGREGATE_BY_PC_SECTOR = Struct::StepValue.new( "Postcode region", :pcSector )
+  AGGREGATE_BY_PC_DISTRICT = Struct::StepValue.new( "Postcode district", :pcDistrict )
+  AGGREGATE_BY_NONE = Struct::StepValue.new( "Don't aggregate, just show the total", :none )
+
+  def initialize
+    super( :select_aggregation_type, :aggregate, :radio )
+  end
+
+  def values( workflow )
+    avg_price_values( workflow )
+  end
+
+  def avg_price_values( workflow )
+    case workflow.state( :areaType )
+    when "country"
+      [AGGREGATE_BY_REGION, AGGREGATE_BY_COUNTY, AGGREGATE_BY_DISTRICT,
+       AGGREGATE_BY_PC_DISTRICT, AGGREGATE_BY_PC_SECTOR,
+       AGGREGATE_BY_NONE]
+    when "region"
+      [AGGREGATE_BY_COUNTY, AGGREGATE_BY_DISTRICT,
+       AGGREGATE_BY_PC_AREA, AGGREGATE_BY_PC_DISTRICT, AGGREGATE_BY_PC_SECTOR,
+       AGGREGATE_BY_NONE]
+    when "county"
+      [AGGREGATE_BY_DISTRICT,
+       AGGREGATE_BY_PC_AREA, AGGREGATE_BY_PC_DISTRICT, AGGREGATE_BY_PC_SECTOR,
+       AGGREGATE_BY_NONE]
+    end
+  end
+
+  def traverse( workflow )
+    simple_traverse( workflow, :select_dates )
+  end
+end
---
+++ test/features/show_geography_aggregation_selection_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+feature "show geography aggregation selection" do
+  scenario "visit the workflow step page for country level report" do
+    visit report_design_path( {rt: :avgPrice, areaType: "country"} )
+    page.must_have_css( ".container h1" )
+    page.must_have_css( ".container form[method=get]" )
+    page.must_have_css( ".container .select-aggregation-type" )
+    page.must_have_css( ".container input.button[type=submit]")
+
+    %i{  region county district pcDistrict pcSector }.each do |area_type|
+      page.must_have_css( ".container input[type=radio][value=#{area_type}]")
+    end
+
+    %i{ country pcArea  }.each do |area_type|
+      page.wont_have_css( ".container input[type=radio][value=#{area_type}]")
+    end
+  end
+end
