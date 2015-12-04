5eacd508c34430862a715bed0246c14e39b105de
Ian Dickinson <i.j.dickinson@gmail.com>
2015-12-04 19:17:27 +0000

    Some fine-tuning of the report download page. Format has been removed as a user-choice, links to both files are now provided from the download page. Formatting of status entries is now more consistent.

--- app/helpers/download_request_helper.rb
+++ app/helpers/download_request_helper.rb
@@ -1,13 +1,21 @@
 module DownloadRequestHelper
-  def render_report_request( request, format )
+  def render_report_request( request )
     content_tag( :li, class: "report-status" ) do
+      concat( report_id( request ) )
+      concat( tag( :br  ))
+      concat( request_status( request ) )
+    end
+  end
+
+  def request_status( request )
+    capture do
       case
       when request.unknown?
         concat render_unknown_request( request )
       when request.failed?
         concat render_failed_request( request )
       when request.completed?
-        concat render_completed_request( request, format )
+        concat render_completed_request( request )
       when request.in_progress?
         concat render_in_progress_request( request )
       else
@@ -29,31 +37,29 @@ module DownloadRequestHelper
     end
   end
 
-  def render_completed_request( request, format )
+  def render_completed_request( request )
     content_tag( :span, class: "success" ) do
-      concat( "Report ready:" )
-      concat( link_to( request.label, request.url( format ) ) )
+      concat( "Ready: " )
+      concat( tag( :br ) )
+      concat( "download " )
+      concat( link_to( "Microsoft Excel format", request.url( :xlsx ) ) )
+      concat( tag( :br ) )
+      concat( "download " )
+      concat( link_to( "open-data (csv) format", request.url( :csv ) ) )
     end
   end
 
   def render_in_progress_request( request )
-    remaining = distance_of_time_in_words( request.eta / 1000, 0, include_seconds: true )
-    content_tag( :span, class: "in-progress" ) do
-      concat( content_tag( :span, class: "report-label" ) do
-        request.label
-      concat( tag( :br ))
-      concat "In progress, estimated complete in #{remaining}."
-      end )
-    end
+    eta = request.eta || 0
+    remaining = distance_of_time_in_words( eta / 1000, 0, include_seconds: true )
+    content_tag( :span, "in progress, estimated complete in #{remaining}.", class: "in-progress" )
   end
 
   def render_pending_request( request )
-    content_tag( :span, class: "pending" ) do
-      concat( content_tag( :span, class: "report-label" ) do
-        request.label
-      concat( tag( :br ))
-      concat( "Waiting to start. Position in queue: #{request.position}")
-      end )
-    end
+    content_tag( :span, "waiting to start. Position in queue: #{request.position}", class: "pending" )
+  end
+
+  def report_id( request )
+    content_tag( :span, request.label, {class: "report-id"} )
   end
 end
