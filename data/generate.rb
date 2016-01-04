b8c3f99bc489f89be87c82802eaacb674a89ae6f
Ian Dickinson <i.j.dickinson@gmail.com>
2016-01-04 13:38:53 +0000

    For FS-35, convert list of county and district names extracted from the store into JavaScript

---
+++ data/generate.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/ruby
+# Generator script to convert the region, county, etc, names that @DaveReynolds
+# extracted from the triple store into a JavaScript object that we can use in
+# client-side code.
+
+def file_header
+  <<EOF
+window.lr = window.lr || {};
+window.lr.data = (function() {
+  "use strict";
+  return {
+EOF
+end
+
+def file_footer
+  <<EOF
+  };
+})();
+EOF
+end
+
+def transcribe_file( output, key, datafile )
+  output << "  #{key}: ["
+  open( datafile ).each_with_index do |name, i|
+    output << ", " unless i == 0
+    output << formatted_name( name.strip )
+  end
+  output << "  ]"
+end
+
+def formatted_name( name )
+  " {value: #{name.inspect}, label: #{label_for( name ).inspect}}"
+end
+
+def label_for( name )
+  name
+    .split( " " )
+    .map( &:capitalize )
+    .join( " " )
+end
+
+file = ARGV[1] || "../app/assets/javascripts/data.js"
+geographies = [:county,:district,:region]
+
+File.open( file, "w" ) do |f|
+  f << file_header
+  geographies.each_with_index do |geography, i|
+    transcribe_file( f, geography, "#{geography}-names.txt" )
+    f << (i == geographies.length - 1 ? "\n" : ",\n")
+  end
+  f << file_footer
+end
