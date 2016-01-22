24534d9616a47bd996e5269bc0d00275d27cf510
Ian Dickinson <i.j.dickinson@gmail.com>
2016-01-22 16:43:21 +0000

    Fix for #7. Option from 'existing' to 'old'. Also re-ordered the options to put the common case first.

--- app/models/step_select_options.rb
+++ app/models/step_select_options.rb
@@ -8,9 +8,9 @@ class StepSelectOptions < Step
 
   def values_options( workflow = nil )
     [
-      ["New-build properties only", :new],
-      ["Existing properties only", :old],
       ["Both old and new properties", :any]
+      ["New-build properties only", :new],
+      ["Old properties only", :old],
     ]
   end
 
