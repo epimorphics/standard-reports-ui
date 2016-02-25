80a91066a88e3b5ec47f0a26cc5756219639f353
Ian Dickinson <ian@epimorphics.com>
2016-02-25 15:26:40 +0000

    Fix for #20 - do not rely on XHR detection to determine when to load the partial page

--- app/controllers/download_report_controller.rb
+++ app/controllers/download_report_controller.rb
@@ -1,11 +1,21 @@
 class DownloadReportController < ApplicationController
   def show
+    layout = whole_page?( params ) && "application"
     @report_manager = ReportManager.new( params: params )
-    layout = !request.xhr? && "application"
 
     respond_to do |format|
       format.html { render :show, layout: layout }
       format.json { render json: @report_manager }
     end
+  end
+
+  :private
+
+  def partial_page?( params )
+    params.delete( "_partial" )
+  end
+
+  def whole_page?( params )
+    !partial_page?( params )
   end
 end
