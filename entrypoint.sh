087541945670d2590d76aacc27504998963a7826
der <dave@epimorphics.com>
2022-05-26 12:31:23 +0100

    Debug assistance

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,4 +28,6 @@ fi
 export RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT:-'/app/standard-reports'}
 export SCRIPT_NAME=${RAILS_RELATIVE_URL_ROOT}
 
+echo "{'ts': '`date -u +%FT%T.%3NZ`', 'message': {'text: 'Standard reports booting: env=${RAILS_ENV} RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}', 'level': 'DEBUG'}}"
+
 exec ./bin/rails server -e ${RAILS_ENV} -b 0.0.0.0
