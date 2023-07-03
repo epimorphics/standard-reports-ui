c8e738c4480df7a308912288b2b0d97b3784e105
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2023-07-03 13:41:10 +0100

    feat: Improved caching policy
    This sets the cache control headers to be public and cacheable with a retry limit of 5 mins as per the other cache settings.
    
    https://github.com/epimorphics/hmlr-linked-data/issues/114

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # * Set cache control headers for HMLR apps to be public and cacheable
+  # * Standard Reports uses a time limit of 5 minutes (300 seconds)
+  # Sets the default `Cache-Control` header for all requests,
+  # unless overridden in the action
+  def change_default_caching_policy
+    expires_in 5.minutes, public: true, must_revalidate: true #if Rails.env.production?
+  end
+
 end
