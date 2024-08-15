2aa2e839e56c0eeec4c2dd77039d730956d075c6
Daniel Couzens <dan.couzens@epimorphics.com>
2024-08-15 11:22:59 +0100

    updates js syntax

--- app/assets/javascripts/report_type.js
+++ app/assets/javascripts/report_type.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('details').forEach(function(detail) {
-      const summary = detail.querySelector('summary');
-      const span = summary.querySelector('span.summary');
+      var summary = detail.querySelector('summary');
+      var span = summary.querySelector('span.summary');
   
       detail.addEventListener('toggle', function() {
-        const isHidden = !detail.open;
+        var isHidden = !detail.open;
         span.textContent = span.textContent.replace(
           isHidden ? 'close' : 'view', 
           isHidden ? 'view' : 'close'
