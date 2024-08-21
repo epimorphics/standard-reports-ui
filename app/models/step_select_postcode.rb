2abb14a53b38fdd16f0f654e9089ba1d0f7ddf70
Daniel Couzens <dan.couzens@epimorphics.com>
2024-08-21 09:33:05 +0100

    updates postcode error messages

--- app/models/step_select_postcode.rb
+++ app/models/step_select_postcode.rb
@@ -15,19 +15,6 @@ class StepSelectPostcode < StepSelectArea
     end
   end
 
-  def validate_with(workflow, value)
-    validated_value = validate(value)
-
-    if validated_value
-      workflow.set_state(param_name, validated_value)
-      workflow_update_hook(workflow)
-      workflow.traverse_to(successor_step)
-    else
-      set_flash("Sorry, '#{value}' does not appear to be a valid value for a #{subtype_label}.
-      Perhaps there's a typo or too many characters or maybe an extra space somewhere?")
-    end
-  end
-
   def validate(value)
     normalized_value = value.upcase
     normalized_value =~ validation_pattern && normalized_value
