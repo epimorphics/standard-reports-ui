93545f7d11600c68731b6135c708cd586d2a6a19
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-12-16 17:01:17 +0000

    fix: only trigger instrumentation on internal errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,6 +47,8 @@ class ApplicationController < ActionController::Base
       render_error(400)
     else
       Rails.logger.warn "No explicit error page for exception #{exception} - #{exception.class}"
+      # Instrument ActiveSupport::Notifications for internal server errors only:
+      instrument_internal_error(exception)
       render_error(500)
     end
   end
