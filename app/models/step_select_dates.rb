4f86b3dfc72c513e04a43c60dd777f0b38c5027b
Jon R. Humphrey <jon.humphrey@epimorphics.com>
2024-12-18 11:14:21 +0000

    style: disable rubocop `Style/ArgumentsForwarding`
    as otherwise ruby throws `unexpected token tRPAREN` error

--- app/models/step_select_dates.rb
+++ app/models/step_select_dates.rb
@@ -51,10 +51,10 @@ class StepSelectDates < Step
     'select dates'
   end
 
-  def each_year(hidden_only: true, &block)
+  def each_year(hidden_only: true, &block) # rubocop:disable Style/ArgumentsForwarding
     start_delta = hidden_only ? YEARS_SHOWN_BY_DEFAULT : 0
     delta = Time.current.year - EARLIEST_YEAR
-    (start_delta..delta).each(&block)
+    (start_delta..delta).each(&block) # rubocop:disable Style/ArgumentsForwarding
   end
 
   private
