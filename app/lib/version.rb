04ff9697e4f1c8ce87d2c2b5df6e968bf8ae7395
joescottdave <joseph.davies@outlook.com>
2026-05-18 15:51:18 +0100

    chore: update version

--- app/lib/version.rb
+++ app/lib/version.rb
@@ -3,7 +3,7 @@
 module Version
   MAJOR = 2
   MINOR = 3
-  PATCH = 1
-  SUFFIX = nil
+  PATCH = 2
+  SUFFIX = "prerelease"
   VERSION = "#{MAJOR}.#{MINOR}.#{PATCH}#{SUFFIX && ".#{SUFFIX}"}".freeze
 end
