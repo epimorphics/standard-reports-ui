d5b5994239d9e30fa29c23fa926cc62044dd6539
Daniel Couzens <dan.couzens@epimorphics.com>
2026-07-23 15:05:10 +0100

    Revert "fix: resolve accessibility issues with ARIA attributes, fieldset legends, and alt text"
    This reverts commit 6891f0ae4b4a81081cd20ef3f9602b4d0e347e0c.

--- app/helpers/report_design_helper.rb
+++ app/helpers/report_design_helper.rb
@@ -2,13 +2,12 @@
 
 # :nodoc:
 module ReportDesignHelper # rubocop:disable Metrics/ModuleLength
-  def workflow_step_form(workflow, legend)
+  def workflow_step_form(workflow)
     step = workflow.current_step
 
     form_tag(workflow.form_action, method: 'get') do
       content_tag(:div, class: 'row') do
         concat(content_tag(:fieldset, class: 'col-sm-12 col-md-6') do
-          concat content_tag(:legend, legend, class: 'visually-hidden')
           concat layout_existing_values(workflow)
           concat layout_flash(step)
           concat layout_workflow_form(workflow, step)
